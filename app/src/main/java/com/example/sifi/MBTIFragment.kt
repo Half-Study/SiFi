b9713732162c56a113eadacf3a2233645db4271f
seonwookim0 <https://github.com/seonwookim0.com>
2023-05-25 17:08:47 +0900

    second commit

---
+++ app/src/main/java/com/example/sifi/MBTIFragment.kt
@@ -0,0 +1,27 @@
+package kr.ac.tuk.sifiapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+class MBTIFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view =  inflater.inflate(R.layout.fragment_m_b_t_i, container, false)
+
+
+
+
+        return view
+    }
+
+
+
+}
