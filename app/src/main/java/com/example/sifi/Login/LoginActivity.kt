23e0f856f73f3ce91879f5926d406b8d17252492
정대영 <eodud1025@naver.com>
2023-06-23 19:12:27 +0900

    feat: 임시 저장

--- app/src/main/java/com/example/sifi/Login/LoginActivity.kt
+++ app/src/main/java/com/example/sifi/Login/LoginActivity.kt
@@ -2,32 +2,34 @@ package com.example.sifi.Login
 
 import android.content.ContentValues
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.sifi.MainActivity
 import com.example.sifi.R
 import com.example.sifi.signup.SignUpActivity
-import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.messaging.FirebaseMessaging
 
 class LoginActivity : AppCompatActivity() {
     lateinit var emailEt: EditText
     lateinit var passwordEt: EditText
     lateinit var loginBtn: Button
-    private val auth : FirebaseAuth by lazy {
+    private val auth: FirebaseAuth by lazy {
         FirebaseAuth.getInstance()
     }
-    lateinit var createBtn : Button
+    lateinit var createBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-
+        FirebaseMessaging.getInstance().token.addOnSuccessListener {
+            Log.d("daeYoung", "token: $it")
+        }
 
         emailEt = findViewById(R.id.editEmail)
         passwordEt = findViewById(R.id.editPass)
@@ -35,7 +37,7 @@ class LoginActivity : AppCompatActivity() {
         createBtn = findViewById(R.id.btnCreate)
 
         loginBtn.setOnClickListener {
-            if (emailEt.text.toString().isEmpty() || passwordEt.text.toString().isEmpty()){
+            if (emailEt.text.toString().isEmpty() || passwordEt.text.toString().isEmpty()) {
                 Toast.makeText(this, "email 혹은 password를 반드시 입력하세요.", Toast.LENGTH_SHORT).show()
             } else {
                 auth.signInWithEmailAndPassword(emailEt.text.toString(), passwordEt.text.toString())
@@ -43,7 +45,7 @@ class LoginActivity : AppCompatActivity() {
                         if (task.isSuccessful) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(ContentValues.TAG, "signInWithEmail:success")
-                            if(auth.currentUser!=null){
+                            if (auth.currentUser != null) {
                                 var intent = Intent(this, MainActivity::class.java)
                                 startActivity(intent)
                                 finish()
@@ -52,8 +54,10 @@ class LoginActivity : AppCompatActivity() {
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(ContentValues.TAG, "signInWithEmail:failure", task.exception)
-                            Toast.makeText(baseContext, "로그인 실패",
-                                Toast.LENGTH_SHORT).show()
+                            Toast.makeText(
+                                baseContext, "로그인 실패",
+                                Toast.LENGTH_SHORT
+                            ).show()
 
                         }
                     }
