2abc9f572b953e4ce8d5db19528655d0cfa24b21
정대영 <eodud1025@naver.com>
2023-06-24 20:19:35 +0900

    fix: editText 가려짐 현상 수정

--- app/src/main/java/com/example/sifi/model/Message.kt
+++ app/src/main/java/com/example/sifi/model/Message.kt
@@ -5,5 +5,5 @@ data class Message(
     var id : String = "",        // 메시지 아이디 (자동생성)
     var msg : String = "asd",       // 메시지
     var userName : String = "",  //사용자명
-    var timestamp : Long = 0,    //전송시간 timestamp
+    var timestamp : Long = System.currentTimeMillis(),    //전송시간 timestamp
 )
